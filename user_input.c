9ed0e262d0cbc9d2f6f6f4c069fe5eb586a67eb6
TheGreatday21 <kaizzielijah@gmail.com>
2025-04-26 17:18:43 +0300

    fgets fixed

--- user_input.c
+++ user_input.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
+#include <string.h> 
 
 int main(void)
 {
     char firstname[10] ;//we set a maximum size for the array
     int age;
-    char fullname[23];
+    char fullname[25];
     double height ;
 
     printf("How old are you :\n");
@@ -17,18 +17,22 @@ int main(void)
  
     printf("What is your first name?:\n ");
     scanf("%s", firstname);//with strings we do not need to make a pointer 
-
+//we have to ensure that fgetc is placed after the scanf so that it grabs the \n not grabbed by the scanf function 
+    fgetc(stdin);
     printf("What is your full name :\n");
-    fgets(fullname,25,stdin);
+    fgets(fullname, 25 ,stdin);
 //fgets(variable name , string size here,standard input)
+//the fgets function will also grab the space so to remove the space we use the following command after 
+    //variablename[strlen(variable name) -1 = 0; This can only be used if the library of <string.h>  is also called 
+    fullname[strlen(fullname) - 1 ] = 0;
     
-    printf("Hello, your first name is  %s and your full name is %s \n",firstname,fullname);
+    printf("\nHello, your full name is %s \n",fullname);
     printf("You are %d years old\n",age);
     printf("Your height is %f\n",height);
     return 0;
 }
 
-//scanf is used to receive user input 
+//scanf is used to receive user input . It only grabs characters up to the first white space 
 //we have to precede the variable name with & 
 //To get a line of text from the user we use fgets 
 
